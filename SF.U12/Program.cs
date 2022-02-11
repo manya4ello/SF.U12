e07bbe4484406a9c6e8ddec60a9a3ae11d54090e
Oleg <oleg.freidkin@gmail.com>
2022-02-11 12:51:26 +0300

    Финальная версия

--- SF.U12/Program.cs
+++ SF.U12/Program.cs
@@ -3,7 +3,35 @@
 
 using System;
 
-void Main ()
+namespace SF.U12;
+class Program
 {
+    static void Main()
+    {
+        List<User> users = new List<User>()
+        {   new User("vova1987", "Владимир", true),
+            new User("anka", "Анна", false),
+            new User("soldier1914", "Швейк", true),
+            new User("shurik2020", "Александр", false),
+            new User("fallenangel1963", "Анжела", true),};
 
+        foreach (User user in users)
+        {
+            Console.WriteLine($"Приветствуем Вас, {user.Name}!");
+
+            if (!user.IsPremium)
+                ShowAds();
+        }    
+    }
+    static void ShowAds()
+    {
+        Console.WriteLine("\tПосетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+        Thread.Sleep(1000);
+
+        Console.WriteLine("\tКупите подписку на МыКомбо и слушайте музыку везде и всегда.");        
+        Thread.Sleep(2000);
+
+        Console.WriteLine("\tОформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");    
+        Thread.Sleep(3000);
+    }
 }
---
+++ SF.U12/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF.U12
+{
+	class User
+	{
+		public string Login { get; set; }
+		public string Name { get; set; }
+		public bool IsPremium { get; set; }
+
+		public User(string login, string name, bool ispremium)
+        {
+			Login = login;
+			Name = name;
+			IsPremium = ispremium;
+        }
+	}
+}
